cef001e27648bef800a636aeb9c4ab17be5ce939
Bharanee Rathna <deepfryed@gmail.com>
2011-05-23 13:04:57 +1000

    updated extconf.rb to use pkg-config if available.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,19 @@
 require 'mkmf'
 
-$CFLAGS  = "-I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -Wall"
-$LDFLAGS = "-lglib-2.0"
+$CFLAGS  = %x{pkg-config --cflags glib-2.0}.strip
+$LGFLAGS = %x{pkg-config --libs glib-2.0}.strip
+
+if $CFLAGS.empty?
+  warn %q{WARNING: No pkg-config found for glib-2.0, using defaults. Set GLIB_INCLUDE_DIR env to override.}
+  dirs = ENV.fetch('GLIB_INCLUDE_DIR', '/usr/include/glib-2.0 /usr/lib/glib-2.0/include')
+  $CFLAGS = dirs.split(/\s+/).map {|dir| "-I#{dir}"}.join(' ')
+end
+
+if $LDFLAGS.empty?
+  warn %q{WARNING: No pkg-config found for glib-2.0, using defaults. Set GLIB_LIB env to override.}
+  libs = ENV.fetch('GLIB_LIB', 'glib-2.0')
+  $LDFLAGS = libs.split(/\s+/).map {|lib| "-l#{lib}"}.join(' ')
+end
 
 dir_config("libots", ["/usr/local", "/opt/local", "/usr"])
 
